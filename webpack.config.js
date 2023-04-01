05d6f600055fe676b69ab35e8b561000cccd4101
rastereo <kapturhouse@yandex.ru>
2023-04-01 14:08:26 +0300

    feat: create favicon and optimize images

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -14,6 +16,11 @@ module.exports = {
     filename: '[name].js',
     publicPath: '',
   },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+  },
   mode: 'development',
   devServer: {
     static: path.resolve(__dirname, './dist'),
@@ -23,39 +30,55 @@ module.exports = {
   },
   module: {
     rules: [{
-        test: /\.js$/,
-        use: 'babel-loader',
-        exclude: '/node_modules/'
+      test: /\.js$/,
+      use: 'babel-loader',
+      exclude: '/node_modules/'
+    },
+    {
+      test: /\.(jpe?g|png|gif|svg)$/i,
+      type: "asset",
+      generator: {
+        filename: "images/[name].[hash][ext]"
       },
-      {
-        test: /\.(jpe?g|png|gif|svg|ico)$/,
-        type: 'asset/resource',
-        generator: {
-          filename: "images/[name].[hash][ext]"
-        },
+    },
+    {
+      test: /\.(woff(2)?|eot|ttf|otf)$/,
+      type: 'asset/resource',
+      generator: {
+        filename: "fonts/[name].[hash][ext]"
       },
-        {
-          test: /\.(woff(2)?|eot|ttf|otf)$/,
-          type: 'asset/resource',
-          generator: {
-            filename: "fonts/[name].[hash][ext]"
-          },
-
-        },
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 1
-            }
-          },
-          'postcss-loader'
-        ]
+    },
+    {
+      test: /\.css$/,
+      use: [MiniCssExtractPlugin.loader, {
+        loader: 'css-loader',
+        options: {
+          importLoaders: 1
+        }
       },
+        'postcss-loader'
+      ]
+    },
     ]
   },
   plugins: [
+    new FaviconsWebpackPlugin({
+      logo: './src/images/logo-podpotolcom.svg', // svg works too!
+      mode: 'webapp', // optional can be 'webapp', 'light' or 'auto' - 'auto' by default
+      devMode: 'webapp', // optional can be 'webapp' or 'light' - 'light' by default
+      favicons: {
+        appName: 'Под потолком',
+        appDescription: 'Кровати ручной работы: высокое качество и надёжность',
+        developerName: 'rastereo',
+        developerURL: null, // prevent retrieving from the nearest package.json
+        background: '#ddd',
+        theme_color: '#333',
+        icons: {
+          coast: false,
+          yandex: false
+        }
+      }
+    }),
     new HtmlWebpackPlugin({
       template: './src/index.html',
       filename: 'index.html',
@@ -75,5 +98,46 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: '[name].css'
     }),
-  ]
+  ],
+  optimization: {
+    minimizer: [
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.imageminMinify,
+          options: {
+            // Lossless optimization with custom option
+            // Feel free to experiment with options for better result for you
+            plugins: [
+              ["gifsicle", { interlaced: true }],
+              ["jpegtran", { progressive: true }],
+              ["optipng", { optimizationLevel: 5 }],
+              // Svgo configuration here https://github.com/svg/svgo#configuration
+              [
+                "svgo",
+                {
+                  plugins: [
+                    {
+                      name: "preset-default",
+                      params: {
+                        overrides: {
+                          removeViewBox: false,
+                          addAttributesToSVGElement: {
+                            params: {
+                              attributes: [
+                                { xmlns: "http://www.w3.org/2000/svg" },
+                              ],
+                            },
+                          },
+                        },
+                      },
+                    },
+                  ],
+                },
+              ],
+            ],
+          },
+        },
+      }),
+    ],
+  },
 }
